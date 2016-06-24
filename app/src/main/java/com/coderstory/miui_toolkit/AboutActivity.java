81ed546f17efba73a2de53e89b90a64bd10f6fc0
Your Name <you@example.com>
2016-06-24 21:10:48 +0800

    新增滑动返回插件和新logo

--- app/src/main/java/com/coderstory/miui_toolkit/AboutActivity.java
+++ app/src/main/java/com/coderstory/miui_toolkit/AboutActivity.java
@@ -1,17 +1,23 @@
 package com.coderstory.miui_toolkit;
 
+
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class AboutActivity extends AppCompatActivity {
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
+public class AboutActivity extends SwipeBackActivity {
+    private SwipeBackLayout mSwipeBackLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        mSwipeBackLayout = getSwipeBackLayout();
+        //设置滑动方向，可设置EDGE_LEFT, EDGE_RIGHT, EDGE_ALL, EDGE_BOTTOM
+        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
     }
 
     //打开我的博客
--- app/src/main/java/com/coderstory/miui_toolkit/FAQActivity.java
+++ app/src/main/java/com/coderstory/miui_toolkit/FAQActivity.java
@@ -3,11 +3,17 @@ package com.coderstory.miui_toolkit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FAQActivity extends AppCompatActivity {
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
+public class FAQActivity extends SwipeBackActivity {
+    private SwipeBackLayout mSwipeBackLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        mSwipeBackLayout = getSwipeBackLayout();
+        //设置滑动方向，可设置EDGE_LEFT, EDGE_RIGHT, EDGE_ALL, EDGE_BOTTOM
+        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
     }
 }

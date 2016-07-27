59af70b05e92c94bd3d8f5b781ff6a837eded351
coderstory <coderstory@163.com>
2016-07-27 16:01:38 +0800

    1.7.4

--- app/src/main/java/com/coderstory/miui_toolkit/tools/Filehelper.java
+++ app/src/main/java/com/coderstory/miui_toolkit/tools/Filehelper.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
  public class FileHelper {
-    public  String getFromAssets(String FileName, Context mContext){
+    public   String getFromAssets(String FileName, Context mContext){
         try {
             InputStreamReader inputReader = new InputStreamReader( mContext.getAssets().open(FileName),"utf-8" );
             BufferedReader bufReader = new BufferedReader(inputReader);
